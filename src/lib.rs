b0902ce2a69b97af1b5b53a5ee8c66e5e2e2b9d8
patwie <mail@patwie.com>
2023-05-13 13:45:49 +0200

    Add description and tags

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! This Rust library provides a standalone implementation of the ROS (Robot
+//! Operating System) core. It allows you to run a ROS master and communicate with
+//! other ROS nodes without relying on an external ROS installation. You can use
+//! this library to build ROS nodes entirely in Rust, including publishers and
+//! subscribers, without needing to use any other ROS dependencies.
+//!
+//! # Examples
+//! ```
+//! use url::Url;
+//! async fn demo() -> anyhow::Result<()>{
+//!   const ROS_MASTER_URI: &str = "http://0.0.0.0:11311";
+//!   let uri = Url::parse(ROS_MASTER_URI).unwrap();
+//!   let socket_address = ros_core_rs::url_to_socket_addr(&uri)?;
+//!   let master = ros_core_rs::core::Master::new(&socket_address);
+//!   master.serve().await
+//! }
+//! ```
+//!
 pub mod client_api;
 pub mod core;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
